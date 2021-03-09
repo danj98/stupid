6f86c4d309dd5d4eba92148ee80fe13f081a25c4
luske-dev <dan_joar@hotmail.com>
2021-03-09 13:41:23 +0100

    Robot mode added

--- IdeaMachine.py
+++ IdeaMachine.py
@@ -16,13 +16,18 @@ def get_word(file_name, line_number):
     return line[line_number]
 
 
-def random_idea(noun, verb, noun2):
-    print("A(n) " + noun + " that " + verb + "s", noun2)
+def random_idea(noun, verb, noun2, robot_mode):
+    if robot_mode:
+        print("A Robot that " + verb + "s", noun)
+    else:
+        print("A(n) " + noun + " that " + verb + "s", noun2)
 
 
 rand_n_list = random.sample(range(0, file_length("nouns.txt")), 10)
 rand_v_list = random.sample(range(0, file_length("verbs.txt")), 10)
 rand_n2_list = random.sample(range(0, file_length("nouns.txt")), 10)
+
+robot = True
 
 print("Here are your ideas:")
 print("")
@@ -31,4 +36,4 @@ for i in range(10):
     noun = get_word("nouns.txt", rand_n_list[i]).rstrip("\n")
     verb = get_word("verbs.txt", rand_v_list[i]).rstrip("\n")
     noun2 = get_word("nouns.txt", rand_n2_list[i]).rstrip("\n")
-    random_idea(noun, verb, noun2)
+    random_idea(noun, verb, noun2, robot)
